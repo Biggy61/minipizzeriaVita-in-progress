04a569e6721e2782cb35c0a2a63d2bfe2c2a2db6
Štěpán Žďánský <zdanskystepanek@gmail.com>
2024-05-23 20:35:14 +0200

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+import NavBar from "../../components/NavBar/NavBar";
+import Poster from "../../components/Poster/Poster";
+import Footer from "../../components/Footer/Footer";
+export default function Home(){
+    return(
+        <>
+           <NavBar />
+           <Poster />
+           <Footer />
+        </>
+    )
+}
